8b6ab4e31e6762e676439a151e81383ca3add6ed
noyu38 <nojiyu.38@icloud.com>
2025-06-05 13:40:00 +0900

    Faculty.javaにDepartmentとの関連付けを追加

--- timetable-backend/src/main/java/com/noyu/timetable_backend/model/Faculty.java
+++ timetable-backend/src/main/java/com/noyu/timetable_backend/model/Faculty.java
@@ -1,24 +1,35 @@
 package com.noyu.timetable_backend.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.Objects;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "faculties")
 public class Faculty {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true, length = 100)
     private String name; // 学部名
+
+    @OneToMany(mappedBy = "faculty", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    // mappedBy = "faculty": Departmentエンティティ側の'faculty'フィールドによって関連が管理されることを示す
+    // cascade = CascadeType.ALL: Facultyに対する操作が関連するDepartmentにも伝播する
+    // orphanRemoval = true: FacultyからDepartmentが削除された場合、そのDepartmentもデータベースから削除する
+    private Set<Department> departments = new HashSet<>();
 
     public Faculty() {
 
@@ -44,25 +55,46 @@ public class Faculty {
         this.name = name;
     }
 
+    public Set<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Set<Department> departments) {
+        this.departments = departments;
+    }
+
+    public void addDepartment(Department department) {
+        this.departments.add(department);
+        department.setFaculty(this);
+    }
+
+    public void removeDepartment(Department department) {
+        this.departments.remove(department);
+        department.setFaculty(null);
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (getClass() != o.getClass())
+            return false;
         Faculty faculty = (Faculty) o;
         return Objects.equals(id, faculty.id) &&
-               Objects.equals(name, faculty.name);
+                Objects.equals(name, faculty.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return id != null ? Objects.hash(id) : Objects.hash(name);
     }
 
     @Override
     public String toString() {
         return "Faculty{" +
-               "id = " + id +
-               ", name = " + name + '\'' +
-               "}";
+                "id = " + id +
+                ", name = " + name + '\'' +
+                ", departments_count = " + (departments != null ? departments.size() : 0) +
+                "}";
     }
 }
