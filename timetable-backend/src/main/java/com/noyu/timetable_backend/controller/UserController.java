6947b19c394179860d510de3b051b860ed130235
noyu38 <nojiyu.38@icloud.com>
2025-06-05 17:44:01 +0900

    あっ…

--- timetable-backend/src/main/java/com/noyu/timetable_backend/controller/UserController.java
+++ timetable-backend/src/main/java/com/noyu/timetable_backend/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.noyu.timetable_backend.controller;
 
-import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
