ef593177367eb25d72dd41b86d7d9f75f8e1cbdb
noyu38 <nojiyu.38@icloud.com>
2025-06-06 18:44:03 +0900

    signupとlogin以外のアクセスに認証が必要なように変更

--- timetable-backend/src/main/java/com/noyu/timetable_backend/config/SecurityConfig.java
+++ timetable-backend/src/main/java/com/noyu/timetable_backend/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.noyu.timetable_backend.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -9,28 +8,37 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthEntryPointJwt unauthorizedHandler;
+    private final AuthTokenFilter authTokenFilter;
+
+    @Autowired
+    public SecurityConfig(AuthEntryPointJwt unauthorizedHandler, AuthTokenFilter authTokenFilter) {
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.authTokenFilter = authTokenFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF保護を無効化
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler)) // 認証エラー時のハンドラを設定
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // セッションをステートレスに変更
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        // "/api/**"へのリクエストは認証なしですべて許可する
-                        .requestMatchers("/api/**").permitAll()
-                        // その他のリクエストはすべて認証が必要
-                        .anyRequest().authenticated())
-                // Spring Securityが提供するデフォルトのフォームベースログインを有効にする
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated());
+
+        // 作成したJWT認証フィルターを、UsernamePasswordAuthenticationFilterの前に配置
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
