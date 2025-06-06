25483ec36c927b3c18cd56a7a1108e540fbfb86a
noyu38 <nojiyu.38@icloud.com>
2025-06-06 17:00:17 +0900

    JWTトークンから名前を検索するメソッドとJWTトークンをバリデーションするメソッドをを追加

--- timetable-backend/src/main/java/com/noyu/timetable_backend/security/JwtUtils.java
+++ timetable-backend/src/main/java/com/noyu/timetable_backend/security/JwtUtils.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 
 @Component
 public class JwtUtils {
@@ -41,5 +43,34 @@ public class JwtUtils {
                 .expiration(expityDate) // 有効期限
                 .signWith(key) // 秘密鍵で署名
                 .compact(); // コンパクトな文字列形式に変換
+    }
+
+    // トークンからユーザー名を取得
+    public String getUserNameFromJwtToken(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    // JWTトークンが有効か検証
+    public boolean validateJwtToken(String authToken) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(authToken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("無向なJWT署名です: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("無向なJWTトークンです: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("有効期限切れのJWTトークンです: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claimsストリングが空です: {}", e.getMessage());
+        }
+        return false;
     }
 }
