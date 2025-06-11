f658a73aa23b6b9e8e3664cb9d5a97d339f38208
noyu38 <nojiyu.38@icloud.com>
2025-06-11 15:10:39 +0900

    Course.javaにエンティティアノテーションつけ忘れてた（笑）

--- timetable-backend/src/main/java/com/noyu/timetable_backend/model/Course.java
+++ timetable-backend/src/main/java/com/noyu/timetable_backend/model/Course.java
@@ -1,14 +1,18 @@
 package com.noyu.timetable_backend.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.FetchType;
 import java.util.Objects;
 
+@Entity
+@Table(name = "courses")
 public class Course {
 
     @Id
