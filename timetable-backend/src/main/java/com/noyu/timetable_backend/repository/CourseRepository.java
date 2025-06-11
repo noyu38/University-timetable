dd18a5515dd5fd075ea20c19516be6dea7639d53
noyu38 <nojiyu.38@icloud.com>
2025-06-11 14:28:55 +0900

    学科を検索するためのRepositoryを追加

---
+++ timetable-backend/src/main/java/com/noyu/timetable_backend/repository/CourseRepository.java
@@ -0,0 +1,34 @@
+package com.noyu.timetable_backend.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.noyu.timetable_backend.model.Course;
+
+@Repository
+public interface CourseRepository {
+
+    /**
+     * 指定された学科IDに所属する授業をすべて検索
+     * 
+     * @param departmentId 検索対象の学科ID
+     * @return 見つかった授業のリスト
+     */
+    List<Course> findByDepartmentId(Long departmentId);
+
+    /**
+     * 特定の学科に所属していない授業（全額共通科目など）をすべて検索
+     * 
+     * @return 見つかった授業のリスト
+     */
+    List<Course> findByDepartmentIsNull();
+
+    /**
+     * 授業名に特定の文字列を含む授業を検索（部分一致検索）
+     * 
+     * @param nameKeyword 検索キーワード
+     * @return 見つかった授業のリスト
+     */
+    List<Course> findByNameContaining(String nameKeyword);
+}
