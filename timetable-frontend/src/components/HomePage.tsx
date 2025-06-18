cdb0ddf030ca0b1b6637cfa8843ba24e7de4381b
noyu38 <nojiyu.38@icloud.com>
2025-06-18 23:27:56 +0900

    test: ホームページに時間割のJSONデータを表示させる

--- timetable-frontend/src/components/HomePage.tsx
+++ timetable-frontend/src/components/HomePage.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext"
+import type { TimetableSlotDTO } from "../dto/TImetableDTO";
+import apiClient from "../services/api";
 
 const HomePage = () => {
     const {setToken} = useAuth();
+
+    const [timetable, setTimetable] = useState<TimetableSlotDTO[]>([]);
+    const [error, setError] = useState('');
+
+    // 一度だけ実行されるuseEffect
+    useEffect(() => {
+        const fetchTimetable = async () => {
+            try {
+                // 時間割データを取得
+                const response = await apiClient.get<TimetableSlotDTO[]>("/timetable");
+                setTimetable(response.data);
+            } catch (e) {
+                console.error("時間割の取得に失敗しました: ", e);
+                setError("時間割の取得に失敗しました。");
+            }
+        };
+
+        fetchTimetable(); // 関数を実行
+    }, []); // 第二引数の配列が空のため、一度だけ実行される
 
     // ログアウト処理
     const handleLogout = () => {
@@ -10,9 +32,14 @@ const HomePage = () => {
 
     return (
         <div>
-            <h2>ホームページ</h2>
-            <p>ようこそ！</p>
+            <h2>ホームページ あなたの時間割</h2>
             <button onClick={handleLogout}>ログアウト</button>
+            {error && <p style={{color: "red"}}>{error}</p>}
+
+            <div style={{ textAlign: "left", backgroundColor: "#f0f0f0", padding: "1rem", marginTop: "1rem"}}>
+                <h3>APIから取得したデータ</h3>
+                <pre>{JSON.stringify(timetable, null, 2)}</pre>
+            </div>
         </div>
     );
 };
