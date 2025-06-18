f646bcdc0037d7f1355d1a5bb7c692633ac7d593
noyu38 <nojiyu.38@icloud.com>
2025-06-18 15:45:03 +0900

    react-router-domを使用

--- timetable-frontend/src/App.tsx
+++ timetable-frontend/src/App.tsx
@@ -1,15 +1,31 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage'
+import { useAuth } from './context/AuthContext';
+import HomePage from './components/HomePage';
 
 function App() {
+  const { token } = useAuth();
 
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <Router>
+      <div className='App'>
         <h1>時間割管理アプリ</h1>
-        <LoginPage />
-      </header>
-    </div>
+        <Routes>
+          {/* "/login"パスへのルート定義 */}
+          <Route
+            path='/login'
+            element={token ? <Navigate to="/" /> : <LoginPage />}
+          />
+
+          {/* "/（ホームページ）"パスへのルート定義 */}
+          <Route
+            path='/'
+            element={token ? <HomePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
